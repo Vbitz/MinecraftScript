4881e2090e018684f05da5d26467098504bf74cf
Vbitz <jscarsbrook@gmail.com>
2012-10-22 17:03:52 +1300

    Added Block Texturing. I don't have the IDs for the entire terain.png yet but I will in the future

--- src/common/com/vbitz/MinecraftScript/ScriptedBlock.java
+++ src/common/com/vbitz/MinecraftScript/ScriptedBlock.java
@@ -9,9 +9,22 @@ import net.minecraft.src.Material;
 public class ScriptedBlock extends Block {
 
 	private static HashMap<String, CreativeTabs> tabs = new HashMap<String, CreativeTabs>();
+	public static HashMap<String, Integer> texs = new HashMap<String, Integer>();
+	
+	private static void addTextures(String[] strs) {
+		for (int i = 0; i < strs.length; i++) {
+			texs.put(strs[i], i);
+		}
+	}
 	
 	static {
 		tabs.put("blocks", CreativeTabs.tabBlock);
+		
+		addTextures(new String[] {"grass", "stone", "dirt", "grassSide", "oakWood", "smoothSlabSide", "smoothSlabTop", "brick", "tntSide", "tntTop", "tntBottom", "web", "rose", "flower", "blue", "oakSapling", 
+				"cobblestone", "bedrock", "sand", "gravel", "oakSide", "oakTop", "ironBlock", "goldBlock", "diamondBlock", "emaruldBlock", "empty1", "empty2", "redMushroom", "brownMushroom", "jungleSapling", "fireText1",
+				"goldOre", "ironOre", "coalOre", "bookcase", "mossyCobble", "obsidiun", "biomeGrassSide", "longGrass", "unknown1", "empty3", "empty4", "craftingTop", "furnaceFront", "furnaceSide","dispencerFront", "fireText2",
+				"sponge", "glass", "diamondOre", "redstoneOre", "leavesSide1", "leavesSide2", "stoneBrick", "deadShrub", "fern", "empty5", "empty6", "craftingSide", "craftingSide", "furnaceOnFront", "furnaceTop", "tagiaSapling",
+				"wool", "monstorSpawner", "snow", "ice", "snowGrassSide", "cactusTop", "cactusSide", "cactusBottom", "clay", "reads", "musicBoxSide", "musicBoxTop"});
 	}
 	
 	public ScriptedBlock(int blockId) {
@@ -32,6 +45,14 @@ public class ScriptedBlock extends Block {
 	
 	public void setBlockBrightness(float bright) {
 		this.setLightValue(bright);
+	}
+	
+	public boolean setBlockTexture(String tex) {
+		if (texs.containsKey(tex)) {
+			this.blockIndexInTexture = texs.get(tex);
+			return true;
+		}
+		return false;
 	}
 
 }
--- testing_scripts/test.js
+++ testing_scripts/test.js
@@ -1,3 +1,4 @@
 var blk = api.getScriptedBlock(1);
 blk.setBlockBrightness(0.8);
 blk.setBlockCreativeTab("blocks");
+blk.setBlockTexture("tntTop");
