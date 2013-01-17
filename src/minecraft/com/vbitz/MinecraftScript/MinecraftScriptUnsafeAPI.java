e39eb756d15d07da32b31944f6754e00bf794563
Vbitz <jscarsbrook@gmail.com>
2013-01-17 17:46:25 +1300

    Goodbye to method privacy thanks to a quantum access nuke, also you can now find fields

--- src/minecraft/com/vbitz/MinecraftScript/MinecraftScriptUnsafeAPI.java
+++ src/minecraft/com/vbitz/MinecraftScript/MinecraftScriptUnsafeAPI.java
@@ -1,9 +1,14 @@
 package com.vbitz.MinecraftScript;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import javax.management.relation.Relation;
+
 import org.mozilla.javascript.NativeJavaPackage;
+
+import com.vbitz.MinecraftScript.exceptions.ScriptErrorException;
 
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.ModContainer;
@@ -17,7 +22,45 @@ import net.minecraft.world.World;
  * Tread with caution, this is a massively powerful API
  */
 public class MinecraftScriptUnsafeAPI {
-	public static void dir(Object obj, boolean getPublics, String search) {
+	private static void printMethods(EntityPlayer ply, Class<?> cls, String search, boolean recurse) {
+		for (Method method : cls.getDeclaredMethods()) {
+			if (method.getName().contains(search)) {
+				String args = "";
+				for (Class params : method.getParameterTypes()) {
+					args += params.getName() + ", ";
+				}
+				// welcome to the future, this is a quantum access nuke
+				method.setAccessible(true);
+				ply.sendChatToPlayer("  " + cls.getSimpleName() + " : " + method.getReturnType().getSimpleName() + " " + method.getName() + "( " + args + " );");
+			}
+		}
+		if (cls.getSuperclass() != null || !recurse) {
+			printMethods(ply, cls.getSuperclass(), search, true);
+		}
+	}
+	
+	private static void printFields(EntityPlayer ply, Class<?> cls, String search, boolean recurse) {
+		for (Field method : cls.getDeclaredFields()) {
+			if (method.getName().contains(search)) {
+				String args = "";
+				method.setAccessible(true);
+				ply.sendChatToPlayer("  " + cls.getSimpleName() + " : " + method.getType().getSimpleName() + " " + method.getName() + ";");
+			}
+		}
+		if (cls.getSuperclass() != null || !recurse) {
+			printFields(ply, cls.getSuperclass(), search, true);
+		}
+	}
+	
+	public static void dir(Object obj) {
+		dir(obj, "", true);
+	}
+	
+	public static void dir(Object obj, String search) {
+		dir(obj, search, false);
+	}
+	
+	public static void dir(Object obj, String search, boolean onlySelf) {
 		EntityPlayer ply = ScriptingManager.getScriptRunner();
 		Class realClass = null;
 		if (obj instanceof Class) {
@@ -27,19 +70,10 @@ public class MinecraftScriptUnsafeAPI {
 			ply.sendChatToPlayer("Class Name: " + obj.getClass().getName());
 			realClass = obj.getClass();
 		}
-		if (!getPublics) {
-			return;
-		}
+		ply.sendChatToPlayer("Fields: ");
+		printFields(ply, realClass, search, onlySelf);
 		ply.sendChatToPlayer("Methods: ");
-		for (Method method : realClass.getDeclaredMethods()) {
-			if (method.getName().contains(search)) {
-				String args = "";
-				for (Class params : method.getParameterTypes()) {
-					args += params.getName() + ", ";
-				}
-				ply.sendChatToPlayer("  " + (method.isAccessible() ? "private " : "public ") + method.getReturnType().getSimpleName() + " " + method.getName() + "( " + args + " );");
-			}
-		}
+		printMethods(ply, realClass, search, onlySelf);
 	}
 	
 	public static void fmlMods() {
@@ -66,27 +100,36 @@ public class MinecraftScriptUnsafeAPI {
 		return null;
 	}
 	
-	public void access(Object obj, String itemName) {
-		EntityPlayer ply = ScriptingManager.getScriptRunner();
-		if (obj == null) {
-			return;
-		}
+	public static void call(Object obj, String methodName, Object... args) throws IllegalArgumentException, IllegalAccessException, InvocationTargetException, ScriptErrorException {
 		for (Method method : obj.getClass().getDeclaredMethods()) {
-			if (method.getName().equals(obj)) {
-				if (!method.isAccessible()) {
-					ply.sendChatToPlayer("Making " + method.getName() + " public");
-				}
+			if (method.getName().equals(methodName)) {
 				method.setAccessible(true);
-			}
-		}
-		for (Field field : obj.getClass().getDeclaredFields()) {
-			if (field.getName().equals(obj)) {
-				if (!field.isAccessible()) {
-					ply.sendChatToPlayer("Making " + field.getName() + " public");
+				Class<?>[] params = method.getParameterTypes();
+				Object[] realArgs = new Object[args.length];
+				for (int i = 0; i < realArgs.length; i++) {
+					if (args[i] instanceof Number) {
+						if (args[i].getClass().getName().equals(params[i].getName())) {
+							realArgs[i] = args[i];
+						} else {
+							if (params[i].getSimpleName() == "int") {
+								realArgs[i] = ((Number) args[i]).intValue();
+							} else if (params[i].getSimpleName() == "float") {
+								realArgs[i] = ((Number) args[i]).floatValue();
+							} else if (params[i].getSimpleName() == "double") {
+								realArgs[i] = ((Number) args[i]).doubleValue();
+							} else {
+								MinecraftScriptMod.getLogger().severe(params[i] + " not implamented");
+							}
+						}
+					} else {
+						realArgs[i] = args[i];
+					}
 				}
-				field.setAccessible(true);
+				method.invoke(obj, realArgs);
+				return;
 			}
 		}
+		throw new ScriptErrorException("Method not Found");
 	}
 	
 	public static Object getTileEntity(Vector3f pos) {
